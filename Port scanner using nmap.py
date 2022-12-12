61120ef584b4285da237195e89c9d1d07be79516
Peiman <75386441+peymankhp@users.noreply.github.com>
2022-12-12 14:04:35 +0300

    Port scanner using nmap
    Port scanner using nmap

---
+++ Port scanner using nmap.py
@@ -0,0 +1,28 @@
+import nmap
+
+# take the range of ports to
+# be scanned
+begin = target = input('Please begin Port: ')
+end = target = input('Please end Port: ')
+
+
+# assign the target ip to be scanned to
+# a variable
+target = input('Please type IP: ')
+
+# instantiate a PortScanner object
+scanner = nmap.PortScanner()
+
+for i in range(int(begin),int(end)+1):
+
+	# scan the target port
+	res = scanner.scan(target,str(i))
+
+	# the result is a dictionary containing
+	# several information we only need to
+	# check if the port is opened or closed
+	# so we will access only that information
+	# in the dictionary
+	res = res['scan'][target]['tcp'][i]['state']
+
+	print(f'port {i} is {res}.')
